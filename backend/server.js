1b695f26e5bc68ae04e779990f9ec84409cf49c6
Azad Kumar <azadpurbey2@gmail.com>
2021-02-11 15:04:07 +0530

    basic frontend and backend layout

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+// const express = require('express')
+// const dotenv = require('dotenv')
+// const products = require('./data/products.js')
+
+import express from 'express'
+// import dotenv from 'dotenv'
+// import colors from 'colors'
+// import connectDB from './config/db.js'
+import products from './data/products.js'
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API is running....')
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
+
+// const PORT = process.env.PORT || 5000
+
+// app.listen(
+//   PORT,
+//   console.log(
+//     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+//   )
+// )
+
+app.listen(5000, console.log('server is running on port 5000'))
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Col className='text-center'>Copyright &copr; AzadShop</Col>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
