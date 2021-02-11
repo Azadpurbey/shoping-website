fe063916b2dec31b9f00b81eb06c5465ef4ace91
Azad Kumar <azadpurbey2@gmail.com>
2021-02-12 00:32:10 +0530

    use of redux

---
+++ frontend/src/components/Message.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Alert } from 'react-bootstrap'
+const Message = ({ variant, children }) => {
+  return <Alert variant={variant}>{children}</Alert>
+}
+
+Message.defauktProps = {
+  variant: 'info',
+}
+
+export default Message
